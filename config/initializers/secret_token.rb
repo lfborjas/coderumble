2d4f312dc7afd614261687779bc69938c2633a1a
lfborjas <luis.borjas@escolarea.com>
2010-11-06 16:39:06 -0600

    creado un esqueleto de rails y agregados todos los archivos a git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coderumble::Application.config.secret_token = '65780bb7012091c38807c062278406630883590d0b872d0e8d20fd527711634bbc0bd8ed1150bdc971c7fd97bcc4594a4cfc98904a9d37ec19fdeb29777d2fb3'
