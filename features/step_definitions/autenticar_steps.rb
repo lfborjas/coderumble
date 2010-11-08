4b2f9f39b5b95eeb401d2d29bc25ff8a16cd286d
lfborjas <luis.borjas@escolarea.com>
2010-11-07 23:16:51 -0600

    Creados borradores de los escenarios

--- features/step_definitions/autenticar_steps.rb
+++ features/step_definitions/autenticar_steps.rb
@@ -3,3 +3,19 @@ Dado /^que no estoy autenticado$/ do
   #des-autenticarse. Asumamos que existe y es /users/sign_out
   visit "/users/sign_out"
 end
+
+Dado /^que los siguientes usuarios existen:$/ do |table|
+  table.hashes.each do |info|
+    #la primera vez sería {:username=>"jgalt", :email=>"galt@domain.com", :password=>"foobarbaz"}
+    User.new(info).save!
+  end
+end
+
+Dado /^que estoy autenticado como "([^"]*)"$/ do |username|
+  usuario = User.find_by_username username
+  Entonces %{voy a login}
+  Y %{completo "Email" con "#{usuario.email}"}
+  Y %{completo "Password" con "#{usuario.password}"}
+  Y %{apreto "Sign in"}
+end
+
